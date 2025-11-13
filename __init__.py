6939610219ebe52b04e430a04c7b57d87fa35891
GBieler <guillaume.bieler@gmail.com>
2025-11-13 11:13:26 +0000

    update node version

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __all__ = [
 
 __author__ = """ViewComfy"""
 __email__ = "guillaume@viewcomfy.com"
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 from .src.ViewComfy_Utils.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
